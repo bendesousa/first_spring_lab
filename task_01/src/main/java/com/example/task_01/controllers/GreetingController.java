e0e6f9b530d9909e44978dbd58dd5414ee7d95ab
bendesousa <bdsousa069@gmail.com>
2022-09-05 14:31:38 +0100

    finished greeting controller

--- task_01/src/main/java/com/example/task_01/controllers/GreetingController.java
+++ task_01/src/main/java/com/example/task_01/controllers/GreetingController.java
@@ -1,4 +1,16 @@
 package com.example.task_01.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class GreetingController {
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(value = "name") String name){
+        return "Good afternoon " + name;
+    }
+
+
 }
